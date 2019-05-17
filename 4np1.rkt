cd3b4dc34739b30467e761b8d6e5140dad501dfd
Jose Rene Ortega <ortegaj2@LabImage2016-17>
2019-05-17 15:09:13 -0700

    finished product

--- 4np1.rkt
+++ 4np1.rkt
@@ -4,7 +4,8 @@
 ; // AUTHOR   | Jose Rene Ortega Jr.
 ; // DATE     | May 17th, 2019
 ; // PROJECT  | CSCI301 Programming Assignment 1
-; // FILE     | 4np1.rkt [description].
+; // FILE     | 4np1.rkt reads "range.txt" for two ints on a line as input range, and outputs, to a file, prime numbers
+; //          | of the form 4n+1 to a file.
 ; ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 ; Final Objective:
@@ -21,24 +22,16 @@
 ; This program then outputs to a file named "primes4np1"
 ; ------------------------------------------------------------------------------------------------------------------------
 
-;;; https://groups.csail.mit.edu/mac/ftpdir/scheme-7.4/doc-html/scheme_7.html
-;;; https://docs.racket-lang.org/reference/let.html
-;;; https://docs.racket-lang.org/reference/reader.html#%28part._parse-symbol%29
-;;; https://gist.github.com/matthewp/2324447
-;;; https://www.scheme.com/tspl3/binding.html#./binding:h1
-;;; https://en.wikibooks.org/wiki/Scheme_Programming/Looping
-
 ; splits txt file into characters
-(define (file->char_list path)
- (call-with-input-file path
+(define (file->char_list dir)
+ (call-with-input-file dir
    (lambda (input-port)
      (let loop ((x (read-char input-port)))
        (cond 
         ((eof-object? x) '())
         (#t (begin (cons x (loop (read-char input-port))))))))))
-
 ; splits a string using a character as delimter
-(define (str-split str ch)
+(define (split str ch)
   (let ((len (string-length str)))
     (letrec
       ((split
@@ -51,20 +44,11 @@
                 (else (split a (+ 1 b)))))))
 (split 0 0))))
 
+;binds range integers for use in main input
+(define rangeLow (string->number (first (split (first(split (apply string(file->char_list "range")) #\newline)) #\space))))
+(define rangeHigh (string->number (first (reverse (split (first(split (apply string(file->char_list "range")) #\newline)) #\space)))))
 
-;;;;;;;;;(file->char_list "range.txt")
-;(apply string(file->char_list "range.txt"))
-
-;(let* ([data-noNewLine (str-split (apply string(file->char_list "range.txt")) #\newline)]
-;       [data-result (str-split data-noNewLine #\space)])
-;  data-result)
-
-
-;(data-noNewLine)
- 
-;(str-split (apply string(file->char_list "range.txt")) #\newline)
-;(str-split (apply string(file->char_list "range.txt")) #\space)
-
+;(str-split (first(str-split (apply string(file->char_list "range.txt")) #\newline)) #\space)
 
 ;;; STAGE 1: function that prints vals for a range x -> y
 ; Helper function to append two lists
@@ -110,9 +94,29 @@
 ;;; STAGE 3: Take the primes produced by the function in stage 2 and determine the two squares
 ;;;          that sum to the prime of the form '4n + 1"
 ;;; NEEDS WORK
-(define (squareNum-Sum? num)
-  (define (sum-help h)
-    (cond [(= h num) #f]
-          [(= num (+ (* (floor(sqrt h)) (floor (sqrt h)))
-                     (* (floor (sqrt (- num h))) (floor (sqrt (- num h))))  )) #t]
-          )))
+
+; defines output file, will replace if it exists
+(define out (open-output-file "primes4np1" #:exists 'replace))
+
+; give squares if true
+(define (giveSquares num)    
+(for ([i (xy_range_equation '(0) 0 num )])
+  (for ([j (xy_range_equation '(0) 0 num)])
+    (cond [(and (= (+ (* i i) (* j j)) num) (< i j))
+              (write num out) (display " " out) (write i out) (display " " out) (write j out) (newline out) (newline out)]
+    ))))
+
+; boolean to determine if it is in this form, helps filter range values
+(define (4n+1? n)
+    (cond
+      [(= (modulo (- n 1) 4) 0) #t]
+      [else #f]))
+
+; main function
+( define (main low high)
+   (for ([i (primes (filter 4n+1? (xy_range_equation (list low) low high)))])
+     (giveSquares i))
+   )
+
+(main rangeLow rangeHigh)
+(close-output-port out)
