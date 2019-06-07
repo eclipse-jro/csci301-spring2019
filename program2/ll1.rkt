6000589c7e3bf2f69b6302508ca512a10a7d439f
J. Rene Ortega Jr <atlasbusinessmarketing@gmail.com>
2019-06-06 20:33:25 -0700

    working on input format

--- program2/ll1.rkt
+++ program2/ll1.rkt
@@ -1,13 +1,21 @@
-; 1) program        --> stmt_list $$
-; 2) stmt_list      --> stmt stmt_list
-; 3) smt_list       --> e
-; 4) stmt           --> id := expr
-; 5) stmt           --> read id
-
 ; inputStackTokens (read, A, read, B, C, :=, A, +, B, D)
-; parseStack = (program)
+; parseStack = ("program")
 
-; 
+
+(define id?
+  (let ((bool #f))
+    (lambda (x)
+      (cond
+        ((or (or (or (string=? "A" x) (string=? "B" x)) (string=? "C" x)) (string=? "D" x)) (let ((bool #t)) bool))
+        ((or (or (or (string=? "E" x) (string=? "F" x)) (string=? "G" x)) (string=? "H" x)) (let ((bool #t)) bool))
+        ((or (or (or (string=? "I" x) (string=? "J" x)) (string=? "K" x)) (string=? "L" x)) (let ((bool #t)) bool))
+        ((or (or (or (string=? "M" x) (string=? "N" x)) (string=? "O" x)) (string=? "P" x)) (let ((bool #t)) bool))
+        ((or (or (or (string=? "Q" x) (string=? "R" x)) (string=? "S" x)) (string=? "T" x)) (let ((bool #t)) bool))
+        ((or (or (or (string=? "U" x) (string=? "V" x)) (string=? "W" x)) (string=? "X" x)) (let ((bool #t)) bool))
+        ((or (string=? "Y" x) (string=? "Z" x)) (let ((bool #t)) bool))
+        (else bool)
+        ))))
+
 (define program-table
   (let ((return "ERROR"))
     (lambda (x)
@@ -38,6 +46,70 @@
 (define mult_op-table
   '(0 0 0 0 0 0 0 0 0 18 19 0))
 
-; (cons (cons program-table stmt_list-table) stmt-table)
-(program-table "poke")
+; 19 production rules. Input production ID. Output right side of ID# rule.
+(define production#-to-list
+  (let ((result "ERROR"))
+    (lambda (x)
+      (cond
+        ((string=? "1" x) (let ((result '("stmt_list" "$$"))) result))
+        ((string=? "2" x) (let ((result '("stmt" "stmt_list"))) result))
+        ((string=? "3" x) (let ((result '())) result))
+        ((string=? "4" x) (let ((result '("id" ":=" "expr"))) result))
+        ((string=? "5" x) (let ((result '("read" "id"))) result))
+        ((string=? "6" x) (let ((result '("write" "expr"))) result))
+        ((string=? "7" x) (let ((result '("term" "term_tail"))) result))
+        ((string=? "8" x) (let ((result '("add_op" "term" "term_tail"))) result))
+        ((string=? "9" x) (let ((result '())) result))
+        ((string=? "10" x) (let ((result '("factor" "factor_tail"))) result))
+        ((string=? "11" x) (let ((result '("mult_op" "factor" "factor_tail"))) result))
+        ((string=? "12" x) (let ((result '())) result))
+        ((string=? "13" x) (let ((result '("(" "expr" ")"))) result))
+        ((string=? "14" x) (let ((result '("id"))) result))
+        ((string=? "15" x) (let ((result '("number"))) result))
+        ((string=? "16" x) (let ((result '("+"))) result))
+        ((string=? "17" x) (let ((result '("-"))) result))
+        ((string=? "18" x) (let ((result '("*"))) result))
+        ((string=? "19" x) (let ((result '("/"))) result))
+        (else result)
+        ))))
 
+;;; Define token regonition
+
+(define initial-in-list
+  (let ((infile (open-input-file "input1")))
+    (let f ((x (read-char infile)))
+      (if (eof-object? x)
+          (begin
+            (close-input-port infile)
+            '())
+          (cons x (f (read-char infile))))))
+  )
+
+(define (file-to-char-list dir)
+ (call-with-input-file dir
+   (lambda (input-port)
+     (let recur ((x (read-char input-port)))
+       (cond
+        ((eof-object? x) '())
+        (#t (begin (cons x (recur (read-char input-port))))))))))
+
+(define charlist->stringlist ls)
+  (cond
+    ((empty? ls) empty)
+    ((empty? (cdr ls)) ls)
+    (else
+     (let ((tmp (car ls)))
+       if (equal? tmp (#\r)
+                  (make-string 4 
+                   
+    
+    
+
+;;;----------------------------------------------------------------  MAIN  --------------------------------------------------------------------------------------------
+
+;; "program-table('read'):"
+;; (program-table "read")
+;; "production#-to-list('1'):"
+;; (production#-to-list "1")
+
+(file-to-char-list "test")
